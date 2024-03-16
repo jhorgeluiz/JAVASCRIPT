868d69731d33bac51e510b3ca0c24b4cdf98053b
Jorge Luiz <j87150712@gmail.com>
2024-03-16 19:09:24 -0300

    atualização

--- exercicios/aula012(prática)/script.js
+++ exercicios/aula012(prática)/script.js
@@ -1,1 +1,20 @@
-alert('olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `agora sao ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        img.src = 'manha.png'
+        document.body.style.background = '#C3BCA9'
+    } else if (hora >= 12 && hora <= 18) {
+        //boa tarde
+        img.src = 'tarde.png'
+        document.body.style.background = '#DDDBDE'
+    } else {
+        //boa noite
+        img.src = 'noite.png'
+        document.body.style.background = '#372636'
+    }
+}
