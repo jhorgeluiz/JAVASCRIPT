ddf44db98296bbda53a4d3bc8f7b4514969816e1
Jorge Luiz <j87150712@gmail.com>
2024-03-20 13:27:00 -0300

    aula012 ex015

--- exercicios/aula012(prática)/ex014/script.js
+++ exercicios/aula012(prática)/ex014/script.js
@@ -1,9 +1,10 @@
+
 function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `agora sao ${hora} horas.`
+    msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         //bom dia
         img.src = 'manha.png'
